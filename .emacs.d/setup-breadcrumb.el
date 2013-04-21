3ca4376cc245e64554452241b51c8fb008a26850
it3ration <it3ration>
2013-04-21 16:50:57 -0600

    added keybindings for breadcrumb commands

--- .emacs.d/setup-breadcrumb.el
+++ .emacs.d/setup-breadcrumb.el
@@ -1,11 +1,12 @@
 (require 'breadcrumb)
 
-;; (global-set-key [(shift space)]         'bc-set)            ;; Shift-SPACE for set bookmark
-;; (global-set-key [(meta j)]              'bc-previous)       ;; M-j for jump to previous
-;; (global-set-key [(shift meta j)]        'bc-next)           ;; Shift-M-j for jump to next
-;; (global-set-key [(meta up)]             'bc-local-previous) ;; M-up-arrow for local previous
-;; (global-set-key [(meta down)]           'bc-local-next)     ;; M-down-arrow for local next
-;; (global-set-key [(control c)(j)]        'bc-goto-current)   ;; C-c j for jump to current bookmark
-;; (global-set-key [(control x)(meta j)]   'bc-list)           ;; C-x M-j for the bookmark menu list
+(global-set-key (kbd "C-c b") 'bc-set)
+(global-set-key (kbd "C-c n") 'bc-next)
+(global-set-key (kbd "C-c p") 'bc-previous)
+(global-set-key (kbd "C-c l") 'bc-list)
+(global-set-key (kbd "C-c c") 'bc-clear)
+(global-set-key (kbd "C-c j") 'bc-goto-current)
+(global-set-key (kbd "C-c a") 'bc-local-previous)
+(global-set-key (kbd "C-c e") 'bc-local-next)
 
 (provide 'setup-breadcrumb)

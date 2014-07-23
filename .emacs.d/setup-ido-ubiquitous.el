08ab4894ce8f40c3b64e40c47ae984cbc258b94c
it3ration <it3ration>
2014-07-22 19:51:29 -0600

    put ido-ubiquitous settings in their own setup file

---
+++ .emacs.d/setup-ido-ubiquitous.el
@@ -0,0 +1,6 @@
+(require 'ido-ubiquitous)
+
+;; use ido-mode everywhere
+(ido-ubiquitous-mode 1)
+
+(provide 'setup-ido-ubiquitous)
--- .emacs.d/setup-ido.el
+++ .emacs.d/setup-ido.el
@@ -1,11 +1,7 @@
 (require 'ido)
-(require 'ido-ubiquitous)
 
 ;; turn on ido-mode
 (ido-mode t)
-
-;; use ido-mode everywhere
-(ido-ubiquitous-mode 1)
 
 ;; enable flex matching
 (setq ido-enable-flex-matching t)

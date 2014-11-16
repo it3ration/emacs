828e247cb1ae21f260384505418f4d7fc84f4d77
it3ration <it3ration>
2014-11-16 12:17:15 -0700

    nuked flymake crap in csharp-mode

--- .emacs.d/setup-csharp.el
+++ .emacs.d/setup-csharp.el
@@ -1,7 +1,5 @@
 ;; my specific csharp-mode settings
 (defun it3ration-csharp-style ()
-  (require 'flymake)
-  (flymake-mode)
   (local-set-key (kbd "{") 'c-electric-brace))
 
 (add-hook 'csharp-mode-hook 'it3ration-csharp-style)

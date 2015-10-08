c009838a131de5147dcd3c0fc39302a193e63d25
Kevin Depue <it3ration>
2015-10-07 18:20:36 -0600

    fixed themes

--- .emacs.d/lisp/setup-themes.el
+++ .emacs.d/lisp/setup-themes.el
@@ -1,17 +1,11 @@
-;; (setq custom-theme-directory (concat user-emacs-directory "themes"))
-
-
-;; use my custom it3ration theme
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;; (condition-case nil 
-;;     (load-theme 'it3ration t)
-;;   (wrong-number-of-arguments (load-theme 'it3ration)))
-
 ;; Setup our theme path.
 (setq custom-theme-directory (concat user-emacs-directory "themes"))
 
 ;; Use the it3ration theme for now.
-(load-theme 'it3ration)
+(condition-case nil 
+    (load-theme 'it3ration t)
+  (wrong-number-of-arguments (load-theme 'it3ration)))
 
-(provide 'setup-theme)
+(provide 'setup-themes)
+
 

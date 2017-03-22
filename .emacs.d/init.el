7b9c51ff49c0bc0941c17502fea3d24e997c4a1d
it3ration <it3ration>
2017-03-22 19:46:30 +0100

    Setup c indentation style

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -158,6 +158,30 @@
   (wrong-number-of-arguments (load-theme 'it3ration)))
 
 ;;
+;; Styles
+;;
+
+;; Style defaults.
+(setq-default c-default-style "k&r")
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
+
+;; Specific style settings.
+(defun it3ration-cc-style ()
+  (setq c-default-style "k&r")
+  (c-set-style "k&r")
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+  (setq c-basic-offset 4)
+  (c-set-offset 'inline-open '0)
+  (c-set-offset 'case-label '4)
+  (electric-pair-mode 1))
+
+;; Install the settings.
+(add-hook 'c-mode-common-hook 'it3ration-cc-style)
+
+;;
 ;; Package management
 ;;
 
--- .emacs.d/lisp/setup-font.el
+++
@@ -1,6 +0,0 @@
-(defconst it3ration-font "Menlo Regular-18" "My emacs font.")
-
-;; Make sure we set this for gui emacs.
-(when is-gui (add-to-list 'default-frame-alist `(font . ,it3ration-font)))
-
-(provide 'setup-font)

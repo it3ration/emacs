38bf232fda023bcb5777cae7e5d3f817cf608797
it3ration <it3ration>
2017-03-23 00:21:48 +0100

    Setup erc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -573,16 +573,38 @@
 
     ;; Fix the prompt regex.
     (setq eshell-prompt-regexp "^[$] ")))
-  
+
+;;
+;; erc
+;;
+
+(defun chat-load-settings (file)
+  "Tries to load nickname / password as association list."
+  (when (file-exists-p file)
+    (let ((lines (-map 's-trim (s-lines (f-read file 'utf-8)))))
+      (list (nth 0 lines) (nth 1 lines)))))
+
+(defun chat-connect (file server port)
+  "Tries to load credentials from file, connect to server / port."
+  (let ((settings (chat-load-settings file)))
+    (if (listp settings)
+        (erc :server server
+             :port port
+             :nick (nth 0 settings)
+             :password (nth 1 settings))
+      (erc :server server
+           :port port))))
+
+(defun chat ()
+  "Main erc chat."
+  (interactive)
+  (chat-connect "~/.erc" "irc.freenode.net" 6667))
+
 ;;    '(ag
 ;;      restclient
 ;;      )))
 
-;; ;; Setup various packages.
-;; ;; Order matters here!
 ;; (require 'setup-modeline)
-;; (require 'setup-erc)
-
-;; ;; (require 'setup-diminish)
+;; (require 'setup-diminish)
 
 

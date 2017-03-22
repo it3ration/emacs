bebf8aec9f8f44425b01950755a5998b0ec90bf0
it3ration <it3ration>
2017-03-22 19:57:16 +0100

    Setup cc style(s) properly

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -167,19 +167,18 @@
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
 
-;; Specific style settings.
-(defun it3ration-cc-style ()
-  (setq c-default-style "k&r")
-  (c-set-style "k&r")
-  (setq tab-width 4)
-  (setq indent-tabs-mode nil)
-  (setq c-basic-offset 4)
-  (c-set-offset 'inline-open '0)
-  (c-set-offset 'case-label '4)
-  (electric-pair-mode 1))
-
-;; Install the settings.
-(add-hook 'c-mode-common-hook 'it3ration-cc-style)
+;; The style.
+(add-hook
+ 'c-mode-common-hook
+ (lambda ()
+   (setq c-default-style "k&r")
+   (c-set-style "k&r")
+   (setq tab-width 4)
+   (setq indent-tabs-mode nil)
+   (setq c-basic-offset 4)
+   (c-set-offset 'inline-open '0)
+   (c-set-offset 'case-label '4)
+   (electric-pair-mode 1)))
 
 ;;
 ;; Package management

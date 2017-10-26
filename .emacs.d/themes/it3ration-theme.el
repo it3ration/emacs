9bc7e7ef828daf090ab4a5f94b7e7cccbd36a93f
it3ration <it3ration>
2017-10-26 17:26:31 -0600

    Nuke unnecessary code

--- .emacs.d/themes/it3ration-theme.el
+++ .emacs.d/themes/it3ration-theme.el
@@ -24,16 +24,16 @@
   ;; The modeline.
   (setq-default
    mode-line-format
-   '((:eval (propertize "%& " 'face '(:background "#4e4e4e" :foreground "#00af00" :weight bold)))
-     (:eval (propertize "%b " 'face '(:background "#4e4e4e" :foreground "#ffff87" :weight bold)))
-     (:eval (propertize "(%l, %c) " 'face '(:background "#4e4e4e" :foreground "#ff8700" :weight bold)))
-     (:eval (propertize "%m " 'face '(:background "#4e4e4e" :foreground "#af87ff" :weight bold)))
+   '((:eval (propertize "%& " 'face '(:foreground "#00af00" :weight bold)))
+     (:eval (propertize "%b " 'face '(:foreground "#ffff87" :weight bold)))
+     (:eval (propertize "(%l, %c) " 'face '(:foreground "#ff8700" :weight bold)))
+     (:eval (propertize "%m " 'face '(:foreground "#af87ff" :weight bold)))
      (:eval
       (when vc-mode
         (propertize
          (s-trim (substring-no-properties vc-mode))
          'face
-         '(:background "#4e4e4e" :foreground "#00a7ff" :weight bold))))))
+         '(:foreground "#00a7ff" :weight bold))))))
   ;; The theme.
   (custom-theme-set-faces
    'it3ration

900570ec3db139aaf2da68d8a6b6726f29b9fdd5
it3ration <it3ration>
2017-11-21 15:40:01 -0700

    Define M-x chat in ~/.erc now.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -841,27 +841,8 @@
     ;; Let's use a sane prompt please.
     (setq erc-prompt (lambda () (concat "[" (buffer-name) "]")))))
 
-(defun chat-load-settings (file)
-  "Tries to load nickname / password as association list."
-  (when (file-exists-p file)
-    (let ((lines (-map 's-trim (s-lines (f-read file 'utf-8)))))
-      (list (nth 0 lines) (nth 1 lines)))))
-
-(defun chat-connect (file server port)
-  "Tries to load credentials from file, connect to server / port."
-  (let ((settings (chat-load-settings file)))
-    (if (listp settings)
-        (erc :server server
-             :port port
-             :nick (nth 0 settings)
-             :password (nth 1 settings))
-      (erc :server server
-           :port port))))
-
-(defun chat ()
-  "Main erc chat."
-  (interactive)
-  (chat-connect "~/.erc" "irc.freenode.net" 6667))
+;; Load erc credentials.
+(load "~/.erc" t)
 
 ;;
 ;; restclient

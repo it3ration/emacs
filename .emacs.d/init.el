df75ef6b4f7e68f0aa5623e3007d852f303dc5c5
it3ration <it3ration@users.noreply.github.com>
2018-11-15 18:27:20 -0600

    Make sure we see embedded jsx code in js files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1306,10 +1306,13 @@
          "\\.php")
   :init
   (progn
+    ;; Make sure we see embedded jsx code in js files.
+    (setq web-mode-content-types-alist '(("jsx"  . ".*\\.js[x]?\\'")))
+
+    ;; Indenting offsets.
     (add-hook
      'web-mode-hook
      (lambda ()
-       ;; Indenting offsets.
        (setq web-mode-markup-indent-offset 2)
        (setq web-mode-css-indent-offset 2)
        (setq web-mode-code-indent-offset 2)))))

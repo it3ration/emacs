fbdc46d0600d887e69cea71c7204819b27d9e639
wusticality <1028792+wusticality@users.noreply.github.com>
2020-10-26 15:02:30 +1300

    Change web-mode indent offset to 4

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1422,9 +1422,9 @@
     (add-hook
      'web-mode-hook
      (lambda ()
-       (setq web-mode-markup-indent-offset 2)
-       (setq web-mode-css-indent-offset 2)
-       (setq web-mode-code-indent-offset 2)))))
+       (setq web-mode-markup-indent-offset 4)
+       (setq web-mode-css-indent-offset 4)
+       (setq web-mode-code-indent-offset 4)))))
 
 ;;
 ;; go-mode
